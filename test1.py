9658821096ebe2cc1c7701cc6b800669b4c6fb02
Nicholas Vachon <nicholasvachon@hotmail.com>
2019-03-25 16:08:38 -0400

    added test1.py file

---
+++ test1.py
@@ -0,0 +1,17 @@
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+
+a = [1,2,3,4,5]
+b = [1,3,5,6]
+c = [1,2]
+print(list(set(a).intersection(b)))
+print(list(set(a).union(b)))
+print(set(c) < set(a))
+
