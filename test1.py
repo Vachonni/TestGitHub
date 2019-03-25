5427996bf05e34e5cc0ea844b2957dd311042b9a
Nicholas Vachon <nicholasvachon@hotmail.com>
2019-03-25 16:13:27 -0400

    added 10, 11

--- test1.py
+++ test1.py
@@ -8,8 +8,8 @@ This is a temporary script file.
 
 
 
-a = [1,2,3,4,5]
-b = [1,3,5,6]
+a = [1,2,3,4,5, 10, 11]
+b = [1,3,5,6,11]
 c = [1,2]
 print(list(set(a).intersection(b)))
 print(list(set(a).union(b)))
